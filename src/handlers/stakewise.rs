8680aa7dc31ebf119ed5f9e668a31cb0e8a33271
crisog <ortega.cpp@gmail.com>
2024-06-25 22:59:54 -0400

    fix: stakewise mute error

--- src/handlers/stakewise.rs
+++ src/handlers/stakewise.rs
@@ -48,6 +48,14 @@ async fn ETHVaultRedeemed(ctx: Context) {
     )
     .execute(db)
     .await
+    .map_err(|e| {
+        if let sqlx::Error::Database(db_err) = &e {
+            if db_err.code().as_deref() == Some("23505") {
+                return Ok(());
+            }
+        }
+        Err(e)
+    })
     .unwrap();
 }
 
