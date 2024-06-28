e728a48f7480143f1915d281e8cfa0792facfe26
crisog <ortega.cpp@gmail.com>
2024-06-28 15:45:41 -0400

    fix: stakewise handlers

---
+++ prisma/migrations/20240628194520_/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `vault` on the `StakeWise` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "StakeWise" DROP COLUMN "vault";
--- src/handlers/stakewise.rs
+++ src/handlers/stakewise.rs
@@ -7,7 +7,6 @@ use crate::db;
 async fn ETHVaultDeposited(ctx: Context) {
     let block_number = ctx.log.block_number.unwrap() as i64;
     let log_index = ctx.log.log_index.unwrap() as i64;
-    let vault = event.receiver.to_string();
     let eth = event.assets.to_string();
 
     let block = ctx
@@ -25,11 +24,10 @@ async fn ETHVaultDeposited(ctx: Context) {
     let db = db::get().await;
 
     sqlx::query!(
-        r#"insert into "StakeWise" (block_number, block_timestamp, log_index, vault, eth) values ($1,$2,$3,$4,$5)"#,
+        r#"insert into "StakeWise" (block_number, block_timestamp, log_index, eth) values ($1,$2,$3,$4)"#,
         block_number,
         block_timestamp,
         log_index,
-        vault,
         eth
     )
     .execute(db)
@@ -40,7 +38,6 @@ async fn ETHVaultDeposited(ctx: Context) {
 async fn ETHVaultRedeemed(ctx: Context) {
     let block_number = ctx.log.block_number.unwrap() as i64;
     let log_index = ctx.log.log_index.unwrap() as i64;
-    let vault = event.owner.to_string();
     let eth = format!("-{}", event.assets.to_string());
 
     let block = ctx
@@ -58,11 +55,41 @@ async fn ETHVaultRedeemed(ctx: Context) {
     let db = db::get().await;
 
     sqlx::query!(
-        r#"insert into "StakeWise" (block_number, block_timestamp, log_index, vault, eth) values ($1,$2,$3,$4,$5)"#,
+        r#"insert into "StakeWise" (block_number, block_timestamp, log_index, eth) values ($1,$2,$3,$4)"#,
         block_number,
         block_timestamp,
         log_index,
-        vault,
+        eth
+    )
+    .execute(db)
+    .await.unwrap();
+}
+
+#[handler(ETHVault.Migrated)]
+async fn ETHVaultMigrated(ctx: Context) {
+    let block_number = ctx.log.block_number.unwrap() as i64;
+    let log_index = ctx.log.log_index.unwrap() as i64;
+    let eth = event.assets.to_string();
+
+    let block = ctx
+        .provider
+        .get_block_by_number(
+            BlockNumberOrTag::Number(ctx.log.block_number.unwrap()),
+            false,
+        )
+        .await
+        .unwrap()
+        .unwrap();
+
+    let block_timestamp = block.header.timestamp as i64;
+
+    let db = db::get().await;
+
+    sqlx::query!(
+        r#"insert into "StakeWise" (block_number, block_timestamp, log_index, eth) values ($1,$2,$3,$4)"#,
+        block_number,
+        block_timestamp,
+        log_index,
         eth
     )
     .execute(db)
@@ -83,9 +110,17 @@ async fn VaultsRegistry(ctx: Context) {
 
     ctx.templates
         .start(Template {
-            address: vault,
+            address: vault.clone(),
             start_block: ctx.log.block_number.unwrap(),
             handler: ETHVaultRedeemed::new(),
+        })
+        .await;
+
+    ctx.templates
+        .start(Template {
+            address: vault,
+            start_block: ctx.log.block_number.unwrap(),
+            handler: ETHVaultMigrated::new(),
         })
         .await;
 }
