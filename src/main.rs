071ab7b71db7cc1f0c951aef67bd1f13be11ba39
crisog <ortega.cpp@gmail.com>
2024-07-02 17:04:02 -0400

    feat: loads new indexers

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,35 @@ mod db;
 mod handlers;
 
 use ghost_crab::prelude::*;
-use handlers::stakewise;
-use handlers::rocketpool;
 use handlers::etherfi;
+use handlers::lido;
+use handlers::renzo;
+use handlers::rocketpool;
+use handlers::stakewise;
+use handlers::swell;
 
 #[tokio::main]
 async fn main() {
     let mut indexer = ghost_crab::Indexer::new();
 
-    indexer.load_event_handler(stakewise::VaultsRegistry::new()).await;
-    indexer.load_event_handler(etherfi::EtherFiTVLUpdated::new()).await;
-    indexer.load_block_handler(rocketpool::RocketPoolBlockHandler::new()).await;
+    indexer
+        .load_event_handler(stakewise::VaultsRegistry::new())
+        .await;
+    indexer
+        .load_event_handler(etherfi::EtherFiTVLUpdated::new())
+        .await;
+    indexer
+        .load_block_handler(rocketpool::RocketPoolBlockHandler::new())
+        .await;
+    indexer
+        .load_block_handler(swell::SwellBlockHandler::new())
+        .await;
+    indexer
+        .load_block_handler(lido::LidoBlockHandler::new())
+        .await;
+    indexer
+        .load_block_handler(renzo::RenzoBlockHandler::new())
+        .await;
 
     indexer.start().await;
 }
